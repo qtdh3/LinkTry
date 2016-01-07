7177afc54ce71b2bc82d498cfe841aaecf82d281
Lin_Jf <qtdh3@163.com>
2016-01-07 11:12:31 +0800

    clean some junk code

--- LinkTry/src/com/ljf/linktry/FullscreenActivity.java
+++ LinkTry/src/com/ljf/linktry/FullscreenActivity.java
@@ -2,7 +2,6 @@ package com.ljf.linktry;
 
 import com.ljf.linktry.util.SystemUiHider;
 
-import android.annotation.TargetApi;
 import android.app.Activity;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -11,12 +10,9 @@ import android.graphics.drawable.GradientDrawable;
 import android.graphics.drawable.LayerDrawable;
 import android.graphics.drawable.StateListDrawable;
 import android.media.MediaPlayer;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
@@ -35,49 +31,14 @@ import android.widget.TableRow;
  * @see SystemUiHider
  */
 public class FullscreenActivity extends Activity {
-	/**
-	 * Whether or not the system UI should be auto-hidden after
-	 * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-	 */
-	private static final boolean AUTO_HIDE = true;
-	// private static final boolean AUTO_HIDE = false;
-
-	// /**
-	// * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-	// * user interaction before hiding the system UI.
-	// */
-	// private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
-	/**
-	 * If set, will toggle the system UI visibility upon interaction. Otherwise,
-	 * will show the system UI visibility upon interaction.
-	 */
-	// private static final boolean TOGGLE_ON_CLICK = true;
-	// private static final boolean TOGGLE_ON_CLICK = false;
-
-	/**
-	 * The flags to pass to {@link SystemUiHider#getInstance}.
-	 */
-	// private static final int HIDER_FLAGS =
-	// SystemUiHider.FLAG_HIDE_NAVIGATION;
-	private static final int HIDER_FLAGS = SystemUiHider.FLAG_FULLSCREEN;
-
-	/**
-	 * The instance of the {@link SystemUiHider} for this activity.
-	 */
-	private SystemUiHider mSystemUiHider;
 	private int[] lastBlockPos = null;
 	private int[] lastBlockPosReal = null;
 	private Judgement judgement = null;
 	private int[] LeftRightPosReal = null;
 	private int blockLinesLength;
-
 	private MediaPlayer mediaPlayer;
-
-//	private static final int TABLEVIEWIDNUM=314758;
-
+	
 	private static final int STYLENUM=16;
-
 	private static final String Tag = "MainActivity";
 
 	@Override
@@ -93,41 +54,8 @@ public class FullscreenActivity extends Activity {
 			mediaPlayer.release();
 			mediaPlayer=null;
 		}
-		
-		
-		
-		
-		/*Display display = getWindowManager().getDefaultDisplay();
-		Point outSize = new Point();
-		display.getSize(outSize);
-
-		Rect noStateBarRect = new Rect();
-		getWindow().getDecorView().getWindowVisibleDisplayFrame(noStateBarRect);
-		int stateBarHeit = noStateBarRect.top;
-
-		Rect drawRect = new Rect();
-		getWindow().getDecorView().findViewById(Window.ID_ANDROID_CONTENT)
-				.getDrawingRect(drawRect);
-		Log.d(Tag,
-				"Window.ID_ANDROID_CONTENT："
-						+ getWindow().getDecorView().findViewById(
-								Window.ID_ANDROID_CONTENT));
-		int top = getWindow().getDecorView()
-				.findViewById(Window.ID_ANDROID_CONTENT).getTop();
-		Log.d(Tag,
-				"Screen Height=" + outSize.y + ";stateBarHeit=" + stateBarHeit
-						+ ";noStateBarRect.height=" + noStateBarRect.height()
-						+ ";\ndrawRect.top=" + top + ";drawRect.height"
-						+ drawRect.height());*/
 	}
 	
-//	@Override
-//	protected void onDestroy() {
-//		// TODO Auto-generated method stub
-//		super.onDestroy();
-//		mediaPlayer=null;
-//	}
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -136,81 +64,12 @@ public class FullscreenActivity extends Activity {
 
 		setContentView(R.layout.activity_main);
 
-		// final View controlsView =
-		// findViewById(R.id.fullscreen_content_controls);
-		final View contentView = findViewById(R.id.fullscreen_content);
-
 		final LinearLayout main_Layout = (LinearLayout) findViewById(R.id.main_page_part);
-
-		// Set up an instance of SystemUiHider to control the system UI for
-		// this activity.
-		mSystemUiHider = SystemUiHider.getInstance(this, contentView,
-				HIDER_FLAGS);
-		mSystemUiHider.setup();
-		mSystemUiHider
-				.setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
-					// Cached values.
-					// int mControlsHeight;
-					// int mShortAnimTime;
-
-					@Override
-					@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-					public void onVisibilityChange(boolean visible) {
-						// if (Build.VERSION.SDK_INT >=
-						// Build.VERSION_CODES.HONEYCOMB_MR2) {
-						// // If the ViewPropertyAnimator API is available
-						// // (Honeycomb MR2 and later), use it to animate the
-						// // in-layout UI controls at the bottom of the
-						// // screen.
-						// if (mControlsHeight == 0) {
-						// mControlsHeight = controlsView.getHeight();
-						// }
-						// if (mShortAnimTime == 0) {
-						// // mShortAnimTime = getResources().getInteger(
-						// // android.R.integer.config_shortAnimTime);
-						// mShortAnimTime = 1000;
-						// }
-						// controlsView.animate()
-						// .translationY(visible ? 0 : mControlsHeight)
-						// .setDuration(mShortAnimTime);
-						// } else {
-						// // If the ViewPropertyAnimator APIs aren't
-						// // available, simply show or hide the in-layout UI
-						// // controls.
-						// controlsView.setVisibility(visible ? View.VISIBLE :
-						// View.GONE);
-						// }
-						//
-						// if (visible && AUTO_HIDE) {
-						// // Schedule a hide().
-						// // delayedHide(AUTO_HIDE_DELAY_MILLIS);
-						// delayedHide(1200);
-						// }
-					}
-				});
-
-		// Set up the user interaction to manually show or hide the system UI.
-		// contentView.setOnClickListener(new View.OnClickListener() {
-		// @Override
-		// public void onClick(View view) {
-		// if (TOGGLE_ON_CLICK) {
-		// mSystemUiHider.toggle();
-		// } else {
-		// mSystemUiHider.show();
-		// }
-		// }});
-
-		// Upon interacting with UI controls, delay any scheduled hide()
-		// operations to prevent the jarring behavior of controls going away
-		// while interacting with the UI.
-		// findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
 
 		int[][] TestArray = RandomInit.twoDimenArray(8, 8, STYLENUM);
 		judgement = new Judgement(TestArray,16);
 		TableLayout tableLayout = initBlocksView(TestArray);
-//		tableLayout.setId(TABLEVIEWIDNUM);
 		main_Layout.addView(tableLayout);
-		
 		
 		Button hintButton=new Button(this);
 		hintButton.setText("Hint");
@@ -267,21 +126,6 @@ public class FullscreenActivity extends Activity {
 			}
 		});
 		main_Layout.addView(resetButton);
-		
-//		mediaPlayer=MediaPlayer.create(FullscreenActivity.this, R.raw.ding);
-
-		/*TextView textView = new TextView(this);
-		LinearLayout.LayoutParams lP = new LinearLayout.LayoutParams(-1, 400);
-		lP.height = 400;
-		lP.setMargins(15, 15, 15, 15);
-		textView.setLayoutParams(lP);
-
-		textView.setBackgroundColor(getResources().getColor(
-				com.ljf.linktry.R.color.black_overlay));
-		textView.setText("I am Here");
-		textView.setGravity(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL);
-		// textView.setHeight(100); //该句有效 ，不过在上述LayoutParams 存在时无效
-		main_Layout.addView(textView);*/
 	}
 
 	private View.OnClickListener blocks_onClickListener = new OnClickListener() {
@@ -289,7 +133,6 @@ public class FullscreenActivity extends Activity {
 		@Override
 		public void onClick(View v) {
 			int btnId = v.getId();
-//			Log.d(Tag, "onClickListener:" + btnId);
 			// get the matrix simple coordinate
 			final int[] blockPosNow = new int[2];
 			blockPosNow[0] = btnId / 100;
@@ -304,8 +147,6 @@ public class FullscreenActivity extends Activity {
 			PosAtParentCor[1] = ((View) v.getParent()).getTop();
 			PosAtParentCor[0] += blockLinesLength / 2 + MARGINS_LEFT;
 			PosAtParentCor[1] += blockLinesLength / 2 + MARGINS_TOP;
-//			Log.d(Tag, "LocationInCanvas:" + PosAtParentCor[0] + ","
-//					+ PosAtParentCor[1]);
 
 			boolean isRight = false;
 			if (lastBlockPos != null) {
@@ -325,7 +166,6 @@ public class FullscreenActivity extends Activity {
 									@Override
 									public void run() {
 										try {
-//											MediaPlayer mediaPlayer=MediaPlayer.create(FullscreenActivity.this, R.raw.ding);
 											mediaPlayer.start();
 										} catch (Exception e) {
 											e.printStackTrace();
@@ -334,7 +174,6 @@ public class FullscreenActivity extends Activity {
 									}
 								});
 								thread.start();
-								
 								
 								try {
 									// change simple matrix coordinate into
@@ -356,18 +195,7 @@ public class FullscreenActivity extends Activity {
 									e.printStackTrace();
 								}
 
-								// delay one second then removeBlockAndLine
 								final int[] lastBlockPosCopy = lastBlockPos;
-								
-//								TimerTask timerTask = new TimerTask() {
-//									@Override
-//									public void run() {
-//										removeBlockAndLine(lastBlockPosCopy,
-//												blockPosNow);
-//									}
-//								};
-//								Timer timer = new Timer();
-//								timer.schedule(timerTask, 1500);
 								removeBlockAndLine(lastBlockPosCopy,blockPosNow);
 								isRight = true;
 							}
@@ -395,26 +223,18 @@ public class FullscreenActivity extends Activity {
 
 	private void removeBlockAndLine(final int[] lastBlockPos,
 			final int[] blockPosNow) {
-		// findViewById(0).setAlpha(0);
 		runOnUiThread(new Runnable() {
 			public void run() {
-//				findViewById(100 * lastBlockPos[0] + lastBlockPos[1]).setAlpha(
-//						0);
-//				findViewById(blockPosNow[0] * 100 + blockPosNow[1]).setAlpha(0);
 				final FrameLayout frameLayout = (FrameLayout) findViewById(R.id.framelayoutinside);
 				int childViewCount=frameLayout.getChildCount();
 				Log.d(Tag, "childViewCount="+childViewCount);
 				Animation animation=AnimationUtils.loadAnimation(FullscreenActivity.this, R.anim.disappear);
 				animation.setFillAfter(true);
 				final View shouRemoveView;
-//				if (childViewCount>normalViewCount ) {	
-//					shouRemoveView=frameLayout.getChildAt(normalViewCount - 1);
-//							shouRemoveView.startAnimation(animation);		
-//				}else {
-					shouRemoveView=frameLayout.getChildAt(childViewCount - 1);
-					shouRemoveView.startAnimation(animation);
-					Log.d(Tag, "shouRemoveView"+shouRemoveView);
-//				}
+				shouRemoveView=frameLayout.getChildAt(childViewCount - 1);
+				shouRemoveView.startAnimation(animation);
+				Log.d(Tag, "shouRemoveView"+shouRemoveView);
+					
 				findViewById(100 * lastBlockPos[0] + lastBlockPos[1]).startAnimation(animation);
 				findViewById(blockPosNow[0] * 100 + blockPosNow[1]).startAnimation(animation);
 				frameLayout.postDelayed(new Runnable() {
@@ -427,15 +247,6 @@ public class FullscreenActivity extends Activity {
 						findViewById(blockPosNow[0] * 100 + blockPosNow[1]).setAlpha(0);
 					}
 				}, 1000);
-//				TimerTask timerTask = new TimerTask() {
-//					@Override
-//					public void run() {
-//						frameLayout.removeView(shouRemoveView);
-//						
-//					}
-//				};
-//				Timer timer = new Timer();
-//				timer.schedule(timerTask, 1000);
 			}
 		});
 
@@ -576,48 +387,5 @@ public class FullscreenActivity extends Activity {
 				com.ljf.linktry.R.color.holo_green_light));
 
 		return tableLayout;
-	}
-
-	@Override
-	protected void onPostCreate(Bundle savedInstanceState) {
-		super.onPostCreate(savedInstanceState);
-
-		// Trigger the initial hide() shortly after the activity has been
-		// created, to briefly hint to the user that UI controls
-		// are available.
-		delayedHide(100);
-	}
-
-	/**
-	 * Touch listener to use for in-layout UI controls to delay hiding the
-	 * system UI. This is to prevent the jarring behavior of controls going away
-	 * while interacting with activity UI.
-	 */
-	View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-		@Override
-		public boolean onTouch(View view, MotionEvent motionEvent) {
-			if (AUTO_HIDE) {
-				// delayedHide(AUTO_HIDE_DELAY_MILLIS);
-				delayedHide(100);
-			}
-			return false;
-		}
-	};
-
-	Handler mHideHandler = new Handler();
-	Runnable mHideRunnable = new Runnable() {
-		@Override
-		public void run() {
-			mSystemUiHider.hide();
-		}
-	};
-
-	/**
-	 * Schedules a call to hide() in [delay] milliseconds, canceling any
-	 * previously scheduled calls.
-	 */
-	private void delayedHide(int delayMillis) {
-		mHideHandler.removeCallbacks(mHideRunnable);
-		mHideHandler.postDelayed(mHideRunnable, delayMillis);
 	}
 }
